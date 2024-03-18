75f8c9a451865a61ab5806c838d9352a97cb9ffb
Keshav <keshav0799@gmail.com>
2024-03-18 17:26:02 -0400

    Add forward slash in routes to fix postman error

--- server/routes/route.js
+++ server/routes/route.js
@@ -3,17 +3,17 @@ const router = express.Router();
 const controller = require("../controllers/controller");
 
 //Category Routes
-router.route('api/getAllCategories').get(controller.getAllCategories);
-router.route('api/getCategory').get(controller.getCategory);
-router.route('api/createCategory').post(controller.createCategory);
-router.route('api/deleteCategory').delete(controller.deleteCategory);
+router.route('/api/getAllCategories').get(controller.getAllCategories);
+router.route('/api/getCategory').get(controller.getCategory);
+router.route('/api/createCategory').post(controller.createCategory);
+router.route('/api/deleteCategory').delete(controller.deleteCategory);
 
 
 // Transaction Routes
-router.route('api/getAllTransactions').get(controller.getAllTransactions);
-router.route('api/getTransaction').get(controller.getTransaction);
-router.route('api/createTransaction').post(controller.createTransaction);
-router.route('api/deleteTransaction').delete(controller.deleteTransaction);
+router.route('/api/getAllTransactions').get(controller.getAllTransactions);
+router.route('/api/getTransaction').get(controller.getTransaction);
+router.route('/api/createTransaction').post(controller.createTransaction);
+router.route('/api/deleteTransaction').delete(controller.deleteTransaction);
 
 
 module.exports = router;
