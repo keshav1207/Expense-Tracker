5928fba086b6c5b3cd229f757401aa51d7c2ac37
Keshav <keshav0799@gmail.com>
2024-03-24 21:46:16 -0400

    Create transaction history table dynamically from API

--- client/vite-project/src/pages/TransactionHistoryPage.jsx
+++ client/vite-project/src/pages/TransactionHistoryPage.jsx
@@ -1,8 +1,65 @@
+import { useGetAllTransactionsQuery } from "../redux/api"
+import { MdDelete } from "react-icons/md";
+import { FaEdit } from "react-icons/fa";
+
 
 export default function TransactionHistoryPage(){
+    const{data, isLoading} = useGetAllTransactionsQuery();
+
+
+  if (isLoading) return <div>Loading...</div>
+
+    console.log(data);
+
     return(
         <>
-        <h1>THis is the History Page</h1>
+        <div className="table-container">
+
+        <table>
+
+        <caption> Transaction History</caption>
+
+
+        {/* Add the headers */}
+
+        <thead>
+        <tr>
+        <th>Name</th>
+        <th>Amount</th>
+        <th>Category</th>
+        <th>Date</th>
+        <th></th>
+        <th></th>
+        </tr>
+        </thead>
+
+
+
+        <tbody>
+
+        {/* Add data in table from RTK query */}
+
+        { data ?(data.map((item, index) => (
+        <tr key={index}>
+            <td data-title = "Name">{item.name}</td>
+          <td data-title = "Amount">${item.amount}</td>
+          <td data-title = "Category">{item.Category_Name}</td>
+          {/* Extra formatting for date received from sql  */}
+          <td data-title = "Date">{new Date(item.date).toLocaleDateString()}</td>   
+         <td data-title = "Delete-Icon"><MdDelete /></td> 
+         <td data-title = "Edit-Icon"><FaEdit /></td>
+        </tr>
+        
+        ))): null }
+
+        </tbody>
+
+
+        </table>
+
+        </div>
+        
+        
         </>
     )
 }
