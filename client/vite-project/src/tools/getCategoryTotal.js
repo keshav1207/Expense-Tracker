2736597b695330602ef18fb4918c9091eea6a982
Keshav <keshav0799@gmail.com>
2024-04-15 04:56:48 -0400

    Create getCategoryTotal function

---
+++ client/vite-project/src/tools/getCategoryTotal.js
@@ -0,0 +1,24 @@
+//Function to get the total per each category of transactions
+
+export default function getCategoryTotal(transactions){
+    let categoryTotal = {};
+  
+
+     for(var i in transactions){
+            const categoryName = transactions[i].Category_Name;
+            const amount = transactions[i].amount;
+
+            
+            if (categoryTotal[categoryName]) {
+                categoryTotal[categoryName] += amount;
+            } else {
+                categoryTotal[categoryName] = amount;
+            }
+        
+
+     }
+
+
+    return categoryTotal;
+
+}
