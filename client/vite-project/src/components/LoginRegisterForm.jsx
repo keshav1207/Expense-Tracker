d2d8a6d483a17314622dc88e003e07c3f869dc24
Keshav <keshav0799@gmail.com>
2024-05-02 23:56:53 -0400

    Create and Style LoginRegisterForm

--- client/vite-project/src/components/LoginRegisterForm.jsx
+++ client/vite-project/src/components/LoginRegisterForm.jsx
@@ -1,5 +1,47 @@
+import { useState } from "react"
+
+
 export default function LoginRegisterForm(){
-    return <>
+
+    const[login, setLogin] = useState(true);
     
-    </>
+    function handleClick(){
+
+        setLogin(!login);
+    }
+
+
+    return(
+        <>
+        <div className="logreg-container">
+
+        <form className="login-register-form">
+
+        <div className="heading">
+
+        <div className="headerLog">{login?("Login"):("Register")}</div>
+
+        <div className="underline"></div>
+
+        </div>
+
+
+        {login?(null):(<input type="text" id="name" placeholder="Name" />)}
+        <input type="text"  id="email" placeholder="Email"/>
+        <input type="password"  id="password " placeholder="Password"/>
+
+       {login?(<div className="logRegMessage" onClick={handleClick}> Already have an account? <span className="form-choice">Login</span></div>):(<div className="logRegMessage" onClick={handleClick}> Don't have an account? <span className="form-choice">Register</span></div>)} 
+
+        {login?( <button className="loginBtn">Login</button>):( <button className="registerBtn">Register</button>)}
+       
+       
+
+        </form>
+
+        </div>
+    
+        </>
+
+    )
+   
 }
